cd68b5c44c697c7d0648531a697cfdd669c5f6f4
sura8888 <surasena221108@gmail.com>
2023-08-21 15:04:24 +0900

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,17 @@
 <template>
-
+    <style>
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+</style>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <NuxtLink class="navbar-brand" to="/" style="margin: 0 auto;">
+      <img src="~/assets/img/icon.png" alt="icon" width="50" height="55" class="d-inline-block align-text-top">
+    </NuxtLink>
+  </div>
+</nav>
 </template>
