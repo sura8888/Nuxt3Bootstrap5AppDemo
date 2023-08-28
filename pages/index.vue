8cae9c5192bf67f3508e9c4de1a6166e5ec262d0
sura8888 <surasena221108@gmail.com>
2023-08-28 14:33:36 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,6 @@
 <template>
-    <NuxtLink to="/recipe">About Page</NuxtLink>
+    <NuxtLink to="/recipe">
+        recipe
+        <img src="~/assets/img/kenta.png" alt="icon" width="300" >
+    </NuxtLink>
   </template>
