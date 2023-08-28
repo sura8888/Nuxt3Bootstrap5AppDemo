626012c0a4dbb0acffdcd4f55a3feeb19cef4fbb
sura8888 <surasena221108@gmail.com>
2023-08-28 14:33:28 +0900

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,7 +2,10 @@
 <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
     <NuxtLink class="navbar-brand" to="/" style="margin: 0 auto;">
-      <img src="~/assets/img/icon.png" alt="icon" width="50" height="55" class="d-inline-block align-text-top">
+        <div class="image-text">
+            <img src="~/assets/img/icon.png" alt="icon" width="50" height="55" class="d-inline-block align-text-top">
+            <p>{{ user.name }}</p>
+        </div>
     </NuxtLink>
   </div>
 </nav>
@@ -14,4 +17,11 @@
     left: 0;
     right: 0;
   }
+  .image-text {
+  display: flex;
+  align-items: center;
+}
 </style>
+<script setup>
+ const user = ref(useLoginUser());
+</script>
