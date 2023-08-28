6d06d4fcf636b5498e0bf9f6a42c8bf9a76d4f98
sura8888 <surasena221108@gmail.com>
2023-08-28 16:32:43 +0900

    Update recipe.vue

--- pages/recipe.vue
+++ pages/recipe.vue
@@ -25,47 +25,17 @@
       </div>
     </div>
   </div>
+
   <div class="container mb-5">
     <h2>{{ data.make }}</h2>
+    <div>
+    <ol class="custom-list row row-cols-1 row-cols-sm-2 row-cols-md-4">
+      <li class="col border p-2" v-for="(item, index) in data.making" :key="item.id">
+        <span v-html="item.text"></span>
+    </li>
+    </ol>
+  </div>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">1</span></div>
-        <p>☆をボウルに混ぜ合わせておく。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">2</span></div>
-        <p>別のボウルに★を全て混ぜ合わせておく。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">3</span></div>
-        <p>肉の厚みがある部分を切り開き、厚みを一定にする。<br />鶏肉１枚を２～３等分の大きめに切る。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">4</span></div>
-        <p>１のボウルに鶏肉を浸す。(時間があれば20分以上浸す。)</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">5</span></div>
-        <p>２のボウルに鶏肉を入れて粉類をまんべんなくまぶし、別の容器に並べていく。<br />
-          粉が余るようでしたら、二度付けをお勧めします！</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">6</span></div>
-        <p>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">7</span></div>
-        <p>油を160度に温める。<br />
-          (温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">8</span></div>
-        <p>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</p>
-      </div>
-      <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">9</span></div>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。
-      </div>
-
     </div>
     <ol>
 
@@ -92,6 +62,12 @@
     </div>
   </div>
 </template>
+<style>
+.custom-list {
+  list-style-position: inside; /* リストアイテムの数字を内側に配置 */
+  padding-left: 0; /* リストアイテムの左の余白を削除 */
+}
+</style>
 <script setup>
  const data = useRecipe()
 </script>
